03708f01444a5b810bc273eb93002841c45f999c
OctavianoRyan25 <octavianoryan25@gmail.com>
2023-10-27 14:31:38 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["index.html"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#111827",
+                secondary: "#EEC373",
+                third: "#232D3F",
+            },
+        },
+    },
+    plugins: [],
+};
